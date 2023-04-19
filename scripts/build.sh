9b8003cc5ef2bfbd26d50e900c1f624a1721eca3
Hendrik Wolff <hendrik.wolff@agdsn.me>
2023-04-19 20:22:58 +0200

    Initial commit

---
+++ scripts/build.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+repo_dir=$(readlink -f -- "$(dirname "$(readlink -f -- "$0")")"/../)
+out="$repo_dir/out"
+build="$repo_dir/build"
+ra="$build/rust_analyzer"
+scripts="$repo_dir/scripts"
+
+pandoc_opts=(
+    --standalone
+    --from markdown --to html
+    --metadata title='Reference Documentation for Rust Analyzer Options'
+    --template "$repo_dir/pandoc.html.template"
+    -V "date-meta=$(date +%F)"
+)
+
+clone_ra() {
+    if [[ -d "$ra" ]]; then
+        cd "$ra"
+        git fetch
+    else
+        git clone https://github.com/rust-lang/rust-analyzer.git "$ra"
+    fi
+}
+
+generate_index() {
+    rm -f "$build/index.md" "$build/index.rev.md"
+    for tag in $(git tag); do
+        echo "- [$tag]($tag/index.html)" >> "$build/index.rev.md"
+    done
+    echo "# Rust Analyzer Tags" > "$build/index.md"
+    tac "$build/index.rev.md" >> "$build/index.md"
+}
+
+build_docs_all() {
+    mkdir -p "$build"
+    mkdir -p "$out"
+    clone_ra
+    cd "$ra"
+
+    generate_index
+
+    for tag in $(git tag); do
+        echo "$tag"
+        mkdir -p "$build/$tag"
+        git show "$tag":editors/code/package.json > "$build/$tag/package.json"
+    done
+
+    for tag in $(git tag); do
+        "$scripts/build-markdown.py" "$build/$tag/package.json" "$build/$tag/rendered.md" &
+    done
+    
+    for tag in $(git tag); do
+        mkdir -p "$out/$tag"
+
+        local pandoc_tag_opts=(
+            -V "date=$tag"
+            "$build/$tag/rendered.md"
+            -o "$out/$tag/index.html"
+        )
+        pandoc "${pandoc_opts[@]}" "${pandoc_tag_opts[@]}" &
+    done
+
+    pandoc "${pandoc_opts[@]}" "$build/index.md" -o "$out/index.html"
+}
+
+usage() {
+    cat << EOF
+Usage:
+    all   - build docs for all tags
+    clean - remove all build artifacts
+EOF
+}
+
+
+if [[ $# -eq 1 ]]; then
+    case $1 in
+        all)
+            build_docs_all
+            exit 0;;
+        clean)
+            rm -rf "$out"
+            rm -rf "$build"
+            exit 0;;
+        *)
+            usage
+            exit 1;;
+    esac
+fi
+
+usage
+exit 1
